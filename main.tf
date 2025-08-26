f9b3e8d1b79a555e777181787e9dd885a4d1ef02
Vishu07052025 <vishu07052025@gmail.com>
2025-08-26 19:15:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-  resource "azurerm_resource_group" "RGV1" {
-  name     = "vishuresource2608V1"
-  location = "West Europe"
+  resource "azurerm_resource_group" "RGV2" {
+  name     = "vishuresource2608V2"
+  location = "East Europe"
 }
