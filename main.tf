a5e02df8a96e4baea79073f5daa92c1a632abb09
Vishu07052025 <vishu07052025@gmail.com>
2025-08-26 17:17:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,22 @@
 resource "azurerm_resource_group" "RG" {
-  name     = "Vishu2608"
+  name     = "vishuresource2608"
   location = "West Europe"
+}
+
+resource "azurerm_storage_account" "strg" {
+  name                     = "vishustoraccount2508"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "strgcont" {
+  name                  = "vishucontainer2608"
+  storage_account_id    = azurerm_storage_account.example.id
+  container_access_type = "private"
 }
