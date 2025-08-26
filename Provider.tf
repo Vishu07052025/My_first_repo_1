20093de108245ccfc986f071b95981a85a97244e
Vishu07052025 <vishu07052025@gmail.com>
2025-08-26 17:15:18 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -6,16 +6,16 @@ terraform {
     }
 
   }
-    backend "azurerm" {
-    resource_group_name   = "vishutoday"
-    storage_account_name  = "vishutoday1212"
-    container_name        = "vishutoday-container"
-    key                   = "terraform.tfstatev1"
-  }
+ #   backend "azurerm" {
+  #  resource_group_name   = "vishutoday"
+  #  storage_account_name  = "vishutoday1212"
+  #  container_name        = "vishutoday-container"
+  #  key                   = "terraform.tfstatev1"
+#  }
 }
 
 provider "azurerm" {
   features {}
 
-  subscription_id = "12c917d2-7f23-4c88-917a-e5f711accf74"
+  subscription_id = "990578fc-8f76-4a5c-a229-c75e0d7e7bb9"
 }
