2b59c897dfe890d7b942b2253bd502f123885a05
Vishu07052025 <vishu07052025@gmail.com>
2025-08-26 19:06:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-#resource "azurerm_resource_group" "RG" {
-#  name     = "vishuresource2608"
-#  location = "West Europe"
-#}
-
-resource "azurerm_storage_account" "strg2" {
-  name                     = "vishustoraccount26082"
-  resource_group_name      = "vishuresource2608"
-  location                 = "West Europe"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
+  resource "azurerm_resource_group" "RGV1" {
+  name     = "vishuresource2608V1"
+  location = "West Europe"
 }
